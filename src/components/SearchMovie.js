e85c95db85f073ea039cac1e7f3def565cd802a5
SabiqulHassan13 <sabiqulhassan98@gmail.com>
2020-08-11 17:50:21 +0600

    searchMovie function created

--- src/components/SearchMovie.js
+++ src/components/SearchMovie.js
@@ -1,9 +1,31 @@
 import React from 'react';
 
 const SearchMovie = () => {
+
+    const searchMovies = async (e) => {
+        e.preventDefault();
+        console.log("submitted");
+
+        const api_key = 'e01d68ece81e7a0533343e472598bd3e';
+        const query = 'jurassic park';
+
+        const url = `https://api.themoviedb.org/3/search/movie?api_key=${api_key}&language=en-US&query=${query}&include_adult=false`;
+
+        try {
+            const res = await fetch(url);
+            const data = await res.json();
+            console.log(data);
+        }catch(err) {
+            console.error(err);
+        }
+        
+
+    }
+
+
     return (
         <div>
-            <form action="" className="form">
+            <form className="form" onSubmit={searchMovies}>
                 <label className="label" htmlFor="query">Movie Name</label>
                 <input 
                     className="input" 
@@ -13,7 +35,6 @@ const SearchMovie = () => {
                 />
                 <button type="submit" className="btn">Search</button>
             </form>
-
         </div>
     );
 }
