68fb03d7820238689d5f0ba150a54d9593298477
SabiqulHassan13 <sabiqulhassan98@gmail.com>
2020-08-11 18:11:14 +0600

    state management with hooks

--- src/components/SearchMovie.js
+++ src/components/SearchMovie.js
@@ -1,20 +1,25 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 const SearchMovie = () => {
+
+    // state - input query, movies
+    const [query, setQuery] = useState('');
+    const [movies, setMovies] = useState([]);
 
     const searchMovies = async (e) => {
         e.preventDefault();
         console.log("submitted");
 
         const api_key = 'e01d68ece81e7a0533343e472598bd3e';
-        const query = 'jurassic park';
+        // const query = 'jurassic park';
 
         const url = `https://api.themoviedb.org/3/search/movie?api_key=${api_key}&language=en-US&query=${query}&include_adult=false`;
 
         try {
             const res = await fetch(url);
             const data = await res.json();
-            console.log(data);
+            // console.log(data.results);
+            setMovies(data.results);
         }catch(err) {
             console.error(err);
         }
@@ -31,6 +36,8 @@ const SearchMovie = () => {
                     className="input" 
                     type="text" 
                     name="query"
+                    value={query}
+                    onChange={(e) => setQuery(e.target.value)}
                     placeholder="i.e. Jurassic Park"
                 />
                 <button type="submit" className="btn">Search</button>
