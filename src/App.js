4874c8553f2f738a48353b8cb11242031429ab6b
SabiqulHassan13 <sabiqulhassan98@gmail.com>
2020-08-11 20:45:08 +0600

    Seperate MovieCard Component Created

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 // import logo from './logo.svg';
 // import './App.css';
-import SearchMovie from './components/SearchMovie';
+import SearchMovie from './components/SearchMovie/SearchMovie';
 
 function App() {
   return (
--- src/components/SearchMovie/SearchMovie.js
+++ src/components/SearchMovie/SearchMovie.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import MovieCard from '../MovieCard/MovieCard';
 
 const SearchMovie = () => {
 
@@ -45,20 +46,9 @@ const SearchMovie = () => {
 
             <div className="card-list">
                 { movies.filter(movie => movie.poster_path).map((movie, index) => (
-                    <div className="card" key={movie.id}>
-                        <img 
-                            src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`} 
-                            alt={movie.title + ' poster'}
-                            className="card--image"
-                        />
-                        <div className="card--content">
-                            <h3 className="card--title">{movie.original_title}</h3>
-                            <p><small>RELEASE DATE: {movie.release_date}</small></p>
-                            <p><small>RATING: {movie.vote_average}</small></p>
-                            <p className="card--desc">{movie.overview}</p>
-                        </div>
+                    
+                    <MovieCard movie={movie} key={movie.id} />
 
-                    </div>
                 )) }
             </div>
 
